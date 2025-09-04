e700b9e81c49256c19e65fd0083373137992d8ec
NVRaghuveerChebrolu <nvraghuveerch@gmail.com>
2025-09-04 20:38:26 +0200

    minor modifications

---
+++ Selenium/demo.py
@@ -0,0 +1,10 @@
+str = "raghuveer"
+for int i=str.length;i>=0 i++
+    print(f(str.charAt(i)))
+
+
+def reverse(s):
+    reverse string =""
+    for i in s:char(s):
+        reverse_char = i + reverse_char
+    return reverse_char
